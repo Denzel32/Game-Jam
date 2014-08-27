76e9aba731790dfe8a2663f1a5aff2a399d917da
rob verhoef <16617@ma-web.nl>
2014-08-27 14:13:52 +0200

    Start scherm

---
+++ Assets/Scripts/BackButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackButton : MonoBehaviour {
+
+	void OnGUI () {
+		
+		GUI.Box(new Rect(10,10,100,40),"");
+		
+		if(GUI.Button(new Rect(20,20,80,20), "Back")) {
+			Application.LoadLevel(0);
+		}
+		
+	}
+	
+}
---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+	void OnGUI () {
+
+		GUI.Box(new Rect(110,10,100,180),"Menu");
+
+		if(GUI.Button(new Rect(120,40,80,20), "Start")) {
+			Application.LoadLevel(1);
+		}
+
+		if(GUI.Button(new Rect(120,70,80,20), "Options")) {
+			//Application.LoadLevel(1);
+		}
+
+		if(GUI.Button(new Rect(120,100,80,20), "Help")) {
+			Application.LoadLevel(2);
+		}
+
+		if(GUI.Button(new Rect(120,130,80,20), "Credits")) {
+			Application.LoadLevel(3);
+		}
+
+		if(GUI.Button(new Rect(120,160,80,20), "Exit")) {
+			Application.Quit();
+		}
+	
+	}
+}
