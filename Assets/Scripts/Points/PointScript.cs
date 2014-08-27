d718528dd54f1443519f2aa9bad47c10469140a2
Pepi Dimi <pepi55@live.com>
2014-08-27 16:21:35 +0200

    Core Game works

--- Assets/Scripts/Points/PointScript.cs
+++ Assets/Scripts/Points/PointScript.cs
@@ -21,7 +21,7 @@ public class PointScript : MonoBehaviour {
 			gameObject.transform.position = Randomize.UpdateLocation(-2.0f, 2.0f, -4.5f, 4.5f);
 			gameObject.renderer.material.color = Color.cyan;
 
-			Debug.Log("Transformed");
+			//Debug.Log("Transformed");
 		}
 	}
 }
