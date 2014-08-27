44a10fde545c241cffceba70056a9b50a46361e8
TomVerkerk <Tomverkerk1@hotmail.com>
2014-08-27 15:49:05 +0200

    touch position

--- Assets/Scripts/TouchBehaviour.cs
+++ Assets/Scripts/TouchBehaviour.cs
@@ -4,22 +4,23 @@ using System.Collections;
 public class TouchBehaviour : MonoBehaviour {
 
 	private Vector2 touch;
+	private Vector2 position;
 	private int index;
+	private float touchX;
+	private float touchY;
 
 	// Use this for initialization
 	void Start () {
 		index = 0;
-		Input.multiTouchEnabled = true;
 	}
 	
 	// Update is called once per frame
 	public void touchInput () {
-		foreach (var T in Input.touches) {
-			if(T.phase == TouchPhase.Moved){
-
-				transform.position = T.position;
-			}
-		}
+		touchX = (100/Screen.width)*Input.GetTouch(0).position.x;
+		touchY = (100/Screen.height)*Input.GetTouch(0).position.y;
+		position.x = -2.8f + 5.6f*(touchX/100);
+		position.y = 5 - 10 * (touchY / 100);
+		transform.position = position;
 	}
 
 	void OnCollisionEnter(Collision col){
