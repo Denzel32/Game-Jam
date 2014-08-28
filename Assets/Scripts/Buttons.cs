c0fce5efe1a932c63938d56bb4a7b3cc8ff83876
rob verhoef <16617@ma-web.nl>
2014-08-28 12:15:31 +0200

    Fixed the menu

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -5,25 +5,25 @@ public class Buttons : MonoBehaviour {
 
 	void OnGUI () {
 
-		GUI.Box(new Rect(110,10,100,180),"Menu");
+		GUI.Box(new Rect(Screen.width*0.1f,Screen.height*0.1f,Screen.width*0.8f,Screen.height*0.8f),"Menu");
 
-		if(GUI.Button(new Rect(120,40,80,20), "Start")) {
+		if(GUI.Button(new Rect(Screen.width*0.2f,Screen.height*0.15f,Screen.width*0.6f,Screen.height*0.1f), "Start")) {
 			Application.LoadLevel(1);
 		}
 
-		if(GUI.Button(new Rect(120,70,80,20), "Options")) {
+		if(GUI.Button(new Rect(Screen.width*0.2f,Screen.height*0.275f,Screen.width*0.6f,Screen.height*0.1f), "Options")) {
 			//Application.LoadLevel(1);
 		}
 
-		if(GUI.Button(new Rect(120,100,80,20), "Help")) {
+		if(GUI.Button(new Rect(Screen.width*0.2f,Screen.height*0.4f,Screen.width*0.6f,Screen.height*0.1f), "Help")) {
 			Application.LoadLevel(2);
 		}
 
-		if(GUI.Button(new Rect(120,130,80,20), "Credits")) {
+		if(GUI.Button(new Rect(Screen.width*0.2f,Screen.height*0.525f,Screen.width*0.6f,Screen.height*0.1f), "Credits")) {
 			Application.LoadLevel(3);
 		}
 
-		if(GUI.Button(new Rect(120,160,80,20), "Exit")) {
+		if(GUI.Button(new Rect(Screen.width*0.2f,Screen.height*0.65f,Screen.width*0.6f,Screen.height*0.1f), "Exit")) {
 			Application.Quit();
 		}
 	
